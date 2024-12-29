ae52b2e31cfa24713ec534e5b5da37610d915677
Avinash <avinashraj0918@gmail.com>
2024-12-30 02:12:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+buttons.forEach(function (bu) {
+    console.log(bu)
+    // b.addEventListener('click',function(e){
+    //     console.log(e);
+    //     console.log(e.target);
+    // })
+});
