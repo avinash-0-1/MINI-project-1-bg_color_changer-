03e3794c537dd2efa67ab567e9adbcd2366e9c7a
Avinash <avinashraj0918@gmail.com>
2025-01-02 14:24:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const buttons = document.querySelectorAll('.button')
-const body = document.querySelector('body')
-
-buttons.forEach(function (bu) {
-    console.log(bu)
-    // b.addEventListener('click',function(e){
-    //     console.log(e);
-    //     console.log(e.target);
-    // })
-});
+    const body = document.querySelector('body')
+    
+    buttons.forEach(function (bu) {
+        bu.addEventListener('click',function(e){
+            if(e.target.id === 'brown'){
+                body.style.backgroundColor = 'brown';
+            }
+        })
+        bu.addEventListener('click',function(e){
+            if(e.target.id === 'white'){
+                body.style.backgroundColor = 'white';
+            }
+        })
+        bu.addEventListener('click',function(e){
+            if(e.target.id === 'red'){
+                body.style.backgroundColor = 'red';
+            }
+        })
+        bu.addEventListener('click',function(e){
+            if(e.target.id === 'grey'){
+                body.style.backgroundColor = 'grey';
+            }
+        })
+    })
